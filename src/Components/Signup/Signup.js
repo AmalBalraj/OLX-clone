f26cd7bbef3da7744d935a3b7746d7e37febdaf2
AmalBalraj <amalbalraj99@gmail.com>
2021-08-18 16:31:19 +0530

    firebase setup

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Logo from '../../olx-logo.png';
 import './Signup.css';
 
 export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    console.log(username)
+  }
+
   return (
     <div>
       <div className="signupParentDiv">
         <img width="200px" height="200px" src={Logo}></img>
-        <form>
+        <form onSubmit = {handleSubmit} >
           <label htmlFor="fname">Username</label>
           <br />
           <input
             className="input"
             type="text"
+            value = {username}
+            onChange = {(e)=>setUsername(e.target.value)}
             id="fname"
             name="name"
             defaultValue="John"
@@ -25,6 +37,8 @@ export default function Signup() {
             className="input"
             type="email"
             id="fname"
+            value = {email}
+            onChange = {(e)=>setEmail(e.target.value)}
             name="email"
             defaultValue="John"
           />
@@ -35,6 +49,8 @@ export default function Signup() {
             className="input"
             type="number"
             id="lname"
+            value = {phone}
+            onChange = {(e)=>setPhone(e.target.value)}
             name="phone"
             defaultValue="Doe"
           />
@@ -45,6 +61,8 @@ export default function Signup() {
             className="input"
             type="password"
             id="lname"
+            value = {password}
+            onChange = {(e)=>setPassword(e.target.value)}
             name="password"
             defaultValue="Doe"
           />
---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCdcndgQiIWsKjLO6I13UScT31tWygpSPQ",
+    authDomain: "olx-clone-ddf10.firebaseapp.com",
+    projectId: "olx-clone-ddf10",
+    storageBucket: "olx-clone-ddf10.appspot.com",
+    messagingSenderId: "921106319556",
+    appId: "1:921106319556:web:cf7c82fd8f394e0b992191",
+    measurementId: "G-9SXHBB66LX"
+  };
+
+export default firebase.initializeApp(firebaseConfig)
